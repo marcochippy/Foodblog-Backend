abc539f02f0db7bb5347b1a4bc6b113164d9b0e1
Ankita Malani <ankitamalani2021@gmail.com>
2025-05-30 11:24:15 +0200

    final

--- models/Posts.js
+++ models/Posts.js
@@ -1,1 +1,29 @@
-//PLACEHOLDER
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/index.js';
+
+const Posts = sequelize.define('Posts',{
+    author:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    title:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    content:{
+        type:DataTypes.TEXT,
+        allowNull:false
+    },
+    cover:{
+        type:DataTypes.STRING(555),
+        allowNull:false
+    },
+    // date:{
+    //     type:DataTypes.DATE,
+    //     allowNull:false
+
+    // }
+})
+// Posts.sync();
+
+ export default Posts;
